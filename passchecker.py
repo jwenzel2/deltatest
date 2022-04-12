8e7f277faa13d12278a2302af20dc863bfe659ae
= <jeremiah@jwenzel.net>
2022-04-12 15:40:22 -0500

    fixed bugs

--- passchecker.py
+++ passchecker.py
@@ -19,15 +19,16 @@ Choose 1 or 2 or q: ")
 
 if question == '1':
     if os.path.exists('rockyou.txt'):
-        wordlist = open('rockyou.txt', 'r', encoding='ANSI')
-        index = 0;
-        flag = 0;
-        for line in wordlist:
-            index += 1
-            if pwstring in line:
-                flag = 1
+        lines = 0
+        with open('rockyou.txt', 'r', encoding='ANSI') as fp:
+            for line in fp:
+                word = line.rstrip('\n')
+                if pwstring == word:
+                    flag = 1
+            fp.close()
         if flag == 0:
             print("Password Not In Wordlist!")
+            quit()
         if flag == 1:
             print("Password Found In Wordlist!")
             quit()
