beee62b0527270bdcc3308873beb858c006eeaf5
Jeremiah Wenzel <jeremiah@jwenzel.net>
2023-11-13 18:28:59 -0600

    Update Thermometer_alarm.ino

--- elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
+++ elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
@@ -5,53 +5,65 @@
 #include "pitches.h"
 #include <dht_nonblocking.h>
 #define DHT_SENSOR_TYPE DHT_TYPE_11
+//setup pins
 #define BLUE 5
 #define GREEN 4
 #define RED 2
-
 static const int DHT_SENSOR_PIN = 13;
-DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
-int redValue;
-int greenValue;
-int blueValue;
+int speaker = 3;
 int menupin = 22;
 int pluspin = 24;
 int negativepin = 26;
 int okpin = 28;
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+//variables for the project
+float temperature;
+float humidity;
+float tempF;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+// establish temp ranges
+float lowtemp = 75;
+float hightemp = 82;
+//speaker settings
+int counter = 500;
+int melody[] = {
+  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
+int redValue;
+int greenValue;
+int blueValue;
+//menuvariables
 int buttonStateMenu = 0; 
 int buttonStatePlus = 0;
 int buttonStateNegative = 0;
 int buttonStateOk = 0;
 int currentScreen = 0;
-float lowtemp = 75;
-float hightemp = 82;
 const int numOfScreens = 2;
 int parameters[numOfScreens] = {lowtemp, hightemp};
 String screens[numOfScreens][2] = {{"Low Temp",String(lowtemp)},{"High Temp",String(hightemp)}};
-int ledPin = 2;
-int counter = 200;
-//                BS  E  D4 D5  D6 D7
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-int melody[] = {
-  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
+
+
 
 
 void setup()
 {
+  //setup lcd
   lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("INITIALIZING!");
+  //setup rgb led
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
-  
   digitalWrite(RED, HIGH);
   digitalWrite(GREEN, LOW);
   digitalWrite(BLUE, LOW);
+  //setup buttons
   pinMode(menupin, INPUT_PULLUP);
   pinMode(pluspin, INPUT_PULLUP);
   pinMode(negativepin,INPUT_PULLUP);
   pinMode(okpin,INPUT_PULLUP);
 }
-
+// take a temperature and humidity measurement
 static bool measure_environment( float *temperature, float *humidity )
 {
   static unsigned long measurement_timestamp = millis( );
@@ -69,7 +81,7 @@ static bool measure_environment( float *temperature, float *humidity )
   return( false );
 }
 
-
+//configuration menu in progress
 void printScreen() {
   if (currentScreen == 2) {
     lcd.setCursor(0,0);
@@ -99,6 +111,8 @@ void printScreen() {
 
 void loop()
 {
+ 
+  lcd.setCursor(0,0);
   /*
   // menu code for future
     buttonStateMenu = digitalRead(menupin);
@@ -140,31 +154,9 @@ void loop()
     delay(200);
     }
 */  
-  float temperature;
-  float humidity;
-  float tempF;
   
-  // This is OK
-  //double tempK = log(10000.0 * ((1024.0 / tempReading - 1)));
-  //tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK )) * tempK );       //  Temp Kelvin
- // float tempC = tempK - 273.15;            // Convert Kelvin to Celcius
- // float tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert Celcius to Fahrenheit
-    
-  //  float tempVolts = tempReading * 5.0 / 1024.0;
-//    float tempC = (tempVolts - 0.5) * 10.0;
-//    float tempF = tempC * 9.0 / 5.0 + 32.0;
-  
-
-  // Display Temperature in C
-  lcd.setCursor(0, 0);
-  //lcd.print("Temp         F  ");
-  // Display Temperature in F
-  //lcd.print("Temp         F  ");
-  lcd.setCursor(0, 0);
-  // Display Temperature in C
- 
-
-  if( measure_environment( &temperature, &humidity ) == true )
+// print out a sucessfull measurement on lcd 
+if( measure_environment( &temperature, &humidity ) == true )
   { 
      tempF = temperature * 9.0 / 5.0 + 32.0;
     lcd.print("T:");
@@ -174,15 +166,11 @@ void loop()
     lcd.print("%");
   }
 
- // float tempF_ptr = &tempF;
-
-  // Display Temperature in F
-  //lcd.print(tempF+"F Humid:"+humidity);
 
   lcd.setCursor(0,1);
   delay(1000);
   lcd.print("                ");
-
+//change LED and or beep depending on temperature ranges
   if (tempF < lowtemp)
   {
     greenValue = 255;
@@ -205,7 +193,7 @@ void loop()
   }
   if (tempF > hightemp)
   {
-    tone(3, melody[5], counter);
+    tone(speaker, melody[5], counter);
     lcd.setCursor(0,1);
     lcd.print("Temp Alarm hit        ");
     redValue = 255;
