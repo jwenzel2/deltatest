9dee33768a55d32d627138505439971fd9d8f084
Jeremiah Wenzel <jeremiah@jwenzel.net>
2023-11-18 00:46:33 -0600

    optimized pin usage

--- elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
+++ elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
@@ -4,19 +4,30 @@
 #include <LiquidCrystal.h>
 #include "pitches.h"
 #include <dht_nonblocking.h>
+#include "LedControl.h"
 #define DHT_SENSOR_TYPE DHT_TYPE_11
 //setup pins
-#define BLUE 5
-#define GREEN 4
-#define RED 2
-static const int DHT_SENSOR_PIN = 13;
+#define BLUE 11
+#define GREEN 12
+#define RED 13
+static const int DHT_SENSOR_PIN = 4;
 int speaker = 3;
 int menupin = 18;
 int forwardpin = 28;
 int backwardpin = 30;
 int negativepin = 34;
 int positivepin = 32;
-LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
+int DIN = 48;
+int CS = 50;
+int CLK = 52;
+int RS= 10;
+int LEN = 9;
+int D4 = 8;
+int D5 = 7;
+int D6 = 6;
+int D7 = 5;
+LiquidCrystal lcd(RS, LEN, D4, D5, D6, D7);
+LedControl lc=LedControl(DIN,CLK,CS,1);
 //variables for the project
 float temperature;
 float humidity;
@@ -50,7 +61,8 @@ int parameters[numOfScreens] = {lowtemp, hightemp,0};
 String screens[numOfScreens] = {"Low Temp","High Temp","Exiting..."};
 
 
-
+unsigned long delaytime1=500;
+unsigned long delaytime2=50;
 
 void setup()
 {
@@ -58,6 +70,11 @@ void setup()
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
   lcd.print("INITIALIZING!");
+  //setup dot matrix
+  lc.shutdown(0,false);
+  lc.setIntensity(0,8);
+  lc.clearDisplay(0);
+  
   //setup rgb led
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
@@ -99,9 +116,143 @@ currentScreen = 0;
 lcd.clear();
 menu_active = true;
 }
+//test function start
 
+void writeArduinoOnMatrix() {
+  /* here is the data for the characters */
+  byte a[5]={B01111110,B10001000,B10001000,B10001000,B01111110};
+  byte r[5]={B00010000,B00100000,B00100000,B00010000,B00111110};
+  byte d[5]={B11111110,B00010010,B00100010,B00100010,B00011100};
+  byte u[5]={B00111110,B00000100,B00000010,B00000010,B00111100};
+  byte i[5]={B00000000,B00000010,B10111110,B00100010,B00000000};
+  byte n[5]={B00011110,B00100000,B00100000,B00010000,B00111110};
+  byte o[5]={B00011100,B00100010,B00100010,B00100010,B00011100};
+
+  /* now display them one by one with a small delay */
+  lc.setRow(0,0,a[0]);
+  lc.setRow(0,1,a[1]);
+  lc.setRow(0,2,a[2]);
+  lc.setRow(0,3,a[3]);
+  lc.setRow(0,4,a[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,r[0]);
+  lc.setRow(0,1,r[1]);
+  lc.setRow(0,2,r[2]);
+  lc.setRow(0,3,r[3]);
+  lc.setRow(0,4,r[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,d[0]);
+  lc.setRow(0,1,d[1]);
+  lc.setRow(0,2,d[2]);
+  lc.setRow(0,3,d[3]);
+  lc.setRow(0,4,d[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,u[0]);
+  lc.setRow(0,1,u[1]);
+  lc.setRow(0,2,u[2]);
+  lc.setRow(0,3,u[3]);
+  lc.setRow(0,4,u[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,i[0]);
+  lc.setRow(0,1,i[1]);
+  lc.setRow(0,2,i[2]);
+  lc.setRow(0,3,i[3]);
+  lc.setRow(0,4,i[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,n[0]);
+  lc.setRow(0,1,n[1]);
+  lc.setRow(0,2,n[2]);
+  lc.setRow(0,3,n[3]);
+  lc.setRow(0,4,n[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,o[0]);
+  lc.setRow(0,1,o[1]);
+  lc.setRow(0,2,o[2]);
+  lc.setRow(0,3,o[3]);
+  lc.setRow(0,4,o[4]);
+  delay(delaytime1);
+  lc.setRow(0,0,0);
+  lc.setRow(0,1,0);
+  lc.setRow(0,2,0);
+  lc.setRow(0,3,0);
+  lc.setRow(0,4,0);
+  delay(delaytime1);
+}
+
+/*
+  This function lights up a some Leds in a row.
+ The pattern will be repeated on every row.
+ The pattern will blink along with the row-number.
+ row number 4 (index==3) will blink 4 times etc.
+ */
+void rows() {
+  for(int row=0;row<8;row++) {
+    delay(delaytime2);
+    lc.setRow(0,row,B10100000);
+    delay(delaytime2);
+    lc.setRow(0,row,(byte)0);
+    for(int i=0;i<row;i++) {
+      delay(delaytime2);
+      lc.setRow(0,row,B10100000);
+      delay(delaytime2);
+      lc.setRow(0,row,(byte)0);
+    }
+  }
+}
+
+/*
+  This function lights up a some Leds in a column.
+ The pattern will be repeated on every column.
+ The pattern will blink along with the column-number.
+ column number 4 (index==3) will blink 4 times etc.
+ */
+void columns() {
+  for(int col=0;col<8;col++) {
+    delay(delaytime2);
+    lc.setColumn(0,col,B00100000);
+    delay(delaytime2);
+    lc.setColumn(0,col,(byte)0);
+    for(int i=0;i<col;i++) {
+      delay(delaytime2);
+      lc.setColumn(0,col,B00100000);
+      delay(delaytime2);
+      lc.setColumn(0,col,(byte)0);
+    }
+  }
+}
+
+/* 
+ This function will light up every Led on the matrix.
+ The led will blink along with the row-number.
+ row number 4 (index==3) will blink 4 times etc.
+ */
+void single() {
+  for(int row=0;row<8;row++) {
+    for(int col=0;col<8;col++) {
+      delay(delaytime2);
+      lc.setLed(0,row,col,true);
+      delay(delaytime2);
+      for(int i=0;i<col;i++) {
+        lc.setLed(0,row,col,false);
+        delay(delaytime2);
+        lc.setLed(0,row,col,true);
+        delay(delaytime2);
+      }
+    }
+  }
+}
+//test function end
 void loop()
 {
+
+//testcode start
+/*
+ writeArduinoOnMatrix();
+  rows();
+  columns();
+  single();
+*/
+//testcode end
 
   while (menu_active == true)
 { 
