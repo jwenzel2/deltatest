5121e5ada51a2d6e16c33741d9a6451f86f4ad74
Jeremiah Wenzel <jeremiah@jwenzel.net>
2024-02-20 18:09:24 -0600

    Update Thermometer_alarm.ino
    updated buttons, UI

--- elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
+++ elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
@@ -58,9 +58,9 @@ float tempF;
 int hmatrix;
 //DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 // establish temp ranges
-float coldtemp = 70.0;
-float lowtemp = 75.0;
-float hightemp = 81.0;
+float coldtemp = 40;
+float lowtemp = 50;
+float hightemp = 65;
 bool beep_enabled = true;
 int count;
 //speaker settings
@@ -76,12 +76,12 @@ bool menu_active = false;
 const int numOfScreens = 4;
 int parameters[numOfScreens] = {coldtemp,lowtemp, hightemp,0};
 String screens[numOfScreens] = {"coldtemp","Low Temp","High Temp","Exiting..."};
-  Elegoo_GFX_Button buttons[3]; //0,210,75,30
+  Elegoo_GFX_Button buttons[7]; //0,210,75,30
 int duration = 500;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 unsigned long delaytime1=500;
 unsigned long delaytime2=50;
-int dht_timer = 5000;
+int dht_timer = 2000;
 int beep_timer = 2000;
 int melody_timer = 500;
 unsigned long lastExecutedMillis_1 = 0;
@@ -122,26 +122,40 @@ void setup()
   tft.begin(identifier);
   tft.setRotation(3);
 
-  buttons[1].initButton(&tft,40,220,75,30,BLUE,BLUE,WHITE,"Menu",3);
+  buttons[0].initButton(&tft,20,220,20,20,BLUE,BLUE,WHITE,"+",3);
+  buttons[0].drawButton();
+   buttons[1].initButton(&tft,100,220,20,20,BLUE,BLUE,WHITE,"-",3);
   buttons[1].drawButton();
-  tft.setCursor(0,215);
-  tft.setTextColor(WHITE);
-  //tft.print("MENU");
-  
-  
+   buttons[2].initButton(&tft,130,220,20,20,BLUE,BLUE,WHITE,"+",3);
+  buttons[2].drawButton();
+   buttons[3].initButton(&tft,200,220,20,20,BLUE,BLUE,WHITE,"-",3);
+  buttons[3].drawButton();
+   buttons[4].initButton(&tft,225,220,20,20,BLUE,BLUE,WHITE,"+",3);
+  buttons[4].drawButton();
+   buttons[5].initButton(&tft,295,220,20,20,BLUE,BLUE,WHITE,"-",3);
+  buttons[5].drawButton();
+      //setup buttons and labels
+      tft.setTextColor(GREEN);
+ tft.fillRect(40,205,40,30, BLACK);
+    tft.setCursor(41,210);
+    tft.print(floor(lowtemp),0);
+    tft.fillRect(145,205,40,30, BLACK);
+    tft.setCursor(146,210);
+    tft.print(floor(lowtemp),0);
+      tft.fillRect(240,205,40,30, BLACK);
+    tft.setCursor(241,210);
+    tft.print(floor(hightemp),0);
+    tft.setCursor(20,180);
+    tft.print("Cold");
+    tft.setCursor(140,180);
+    tft.print("Low");
+    tft.setCursor(230,180);
+    tft.print("High");
   //initialize dht
   dht22.begin();
 }
 
-//configuration menu
-void printScreen() {
-//currentScreen = 0;
-delay(500);
-//clear tft
-//tft.reset();
-menu_active = true;
 
-}
 
 void loop()
 {
@@ -159,132 +173,57 @@ void loop()
     p.x = map(p.y, TS_MINX, TS_MAXX, tft.height(), 0);
     p.y = (tft.width()-map(p.x, TS_MINY, TS_MAXY, tft.width(), 0));
    }
-    if (buttons[1].contains(p.y, p.x)) {
+    if (buttons[0].contains(p.y, p.x)) {
       //Serial.print("Pressing: "); Serial.println(b);
-      buttons[1].press(true); 
+      buttons[0].press(true); 
      // tell the button it is pressed
     } else {
-      buttons[1].press(false);  // tell the button it is NOT pressed
+      buttons[0].press(false);  // tell the button it is NOT pressed
     }
-  if (buttons[1].justPressed()) {
-        buttons[1].drawButton(true); 
+  if (buttons[0].justPressed()) {
+        buttons[0].drawButton(true); 
         if (beep_enabled = true)
         {
           beep_enabled = false;
         }
-        else if (beep_enabled = false)
+        else 
         {
-          beep_enabled = true;
+          beep_enabled == true;
         } // draw invert!
   }
-   if (buttons[1].justReleased()) {
+   if (buttons[0].justReleased()) {
       // Serial.print("Released: "); Serial.println(b);
-      buttons[1].drawButton();  // draw normal
+      buttons[0].drawButton();  // draw normal
     }
-//end test code
-  //work with the menu when it has been activated
-  /*needs adaptation to TFT
-  while (menu_active == true)
-  { 
-    if (digitalRead(forwardpin) == LOW)
-    {
-   
-      if (currentScreen < 5)
-      {
-        currentScreen++;
-        delay(500);
-      }
-    
-    }  
-    if (digitalRead(backwardpin) == LOW)
-    {
-      if (currentScreen > 0)
-      {
-        currentScreen--;
-        delay(500);
-      }
-    } 
-    if (digitalRead(positivepin) == LOW)
-    {
-      int value = parameters[currentScreen];
-      value++;
-      delay(500);
-      if (currentScreen == 0)
-      {
-        lowtemp = value;
-      }
-      if (currentScreen == 1)
-      {
-        hightemp = value;
-      }
-      parameters[currentScreen] = value;
-    }
-    if (digitalRead(negativepin) == LOW)
-    {
-      int value = parameters[currentScreen];
-      value--;
-      delay(500);
-      if (currentScreen == 0)
-      {
-        lowtemp = value;
-      }
-      if (currentScreen == 1)
-      {
-       hightemp = value;
-      }
-      parameters[currentScreen] = value;
-    
-    }
-      if (currentScreen == 3) 
-      {
-        menu_active = false;
-        lcd.clear();
-        lcd.setCursor(0,0);
-        lcd.print("RESUMING PROGRAM");
-        delay(500);
-        lcd.clear();
-      }
-    else 
-      {
-        lcd.setCursor(0,0);
-        lcd.print(screens[currentScreen]);
-        lcd.print(":      ");
-        lcd.setCursor(0,1);
-        lcd.print(parameters[currentScreen]);
-      }
-  }
-*/
- //while (menu_active == false)
- //{
-
- //tft.fillScreen(BLACK);
  
   tft.setCursor(0,0);
 // print out a sucessfull measurement on lcd 
 
   //read temp and humidity
- tempF = dht22.readTemperature(true);
+ tempF = dht22.readTemperature();
  humidity = dht22.readHumidity();
  // send humidity data to dot matrix lcd
  //tempF = temperature * 9.0 / 5.0 + 32.0;
  tft.setTextColor(GREEN);
-    tft.println("Temperature:");
+  
   // tft.drawRect(rectx,recty,140,25, BLACK);
   if (currentMillis - lastExecutedMillis_1 >= dht_timer)
   {
     lastExecutedMillis_1 = currentMillis;
-  
+   tft.println("Temperature:");
     tft.fillRect(0,72,110,25, BLACK);
   tft.fillRect(rectx,recty,120,26, BLACK);
     //tft.print(tempF);
    
     tft.print(tempF);
-    tft.println("F");
+    tft.println("C");
     tft.println("Humidity:");
   //   tft.drawRect(rectx,recty,140,50, BLACK);
 
     tft.print(humidity);
     tft.println("%");
+
+
   }
   
  
@@ -311,8 +250,6 @@ void loop()
   }
   if (tempF <= coldtemp)
     {
-      tft.setCursor(0,1);
-      tft.print("Too Cold!        ");
       redValue = 0;
       blueValue = 255;
       greenValue = 0;
@@ -337,9 +274,6 @@ void loop()
     }
       if (tempF > hightemp)
         { 
-     
-         // tft.setCursor(40,41);
-         // tft.print("Temp Alarm hit        ");
           redValue = 255;
           blueValue = 0;
           greenValue = 0;
