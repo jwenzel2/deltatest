e6f747f62b1ea03d4c4731bccc37818f25776435
jwenzel2 <jeremiah@jwenzel.net>
2025-07-01 19:58:18 -0500

    updated code

--- elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
+++ elegooprojects/Thermometer_alarm/Thermometer_alarm.ino
@@ -8,7 +8,11 @@
 #include <DHT.h>
 #include "LedControl.h"
 #include <TouchScreen.h>
+#include <Wire.h>
+#include <DS3231.h>
 
+DS3231 clock;
+RTCDateTime dt;
 
 #define MINPRESSURE 10
 #define MAXPRESSURE 1000
@@ -34,7 +38,7 @@
 #define XP 8   // can be a digital pin
 static const int DHT_SENSOR_PIN = 49;
 DHT dht22(DHT_SENSOR_PIN, DHT22);
-int speaker = 50;
+
 //make color selection easy
 #define	BLACK   0x0000
 #define	BLUE    0x001F
@@ -48,8 +52,7 @@ int speaker = 50;
 Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
 #define USE_Elegoo_SHIELD_PINOUT
 //Elegoo_TFTLCD tft;
-int rectx = 0;
-int recty= 20;
+
 
 //variables for the project
 float temperature;
@@ -61,22 +64,14 @@ int hmatrix;
 float coldtemp = 40;
 float lowtemp = 50;
 float hightemp = 65;
-bool beep_enabled = true;
 int count;
 //speaker settings
 int counter = 1000;
-int melody[] = {
-  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
 int redValue;
 int greenValue;
 int blueValue;
 //menuvariables
-unsigned long menuTriggeredTime = 0;
-bool menu_active = false;
-const int numOfScreens = 4;
-int parameters[numOfScreens] = {coldtemp,lowtemp, hightemp,0};
-String screens[numOfScreens] = {"coldtemp","Low Temp","High Temp","Exiting..."};
-  Elegoo_GFX_Button buttons[7]; //0,210,75,30
+  Elegoo_GFX_Button buttons[6]; //0,210,75,30
 int duration = 500;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 unsigned long delaytime1=500;
@@ -90,6 +85,14 @@ unsigned long lastExecutedMillis_3 = 0;
 int thisNote = 0;
 void setup()
 {
+    Serial.begin(9600);
+  //setup clock
+ clock.begin();
+ 
+ //bool h12 = false;
+ //void setClockMode(bool h12);
+//clock.setDateTime(2025, 7, 1, 18, 24, 15);
+ // clock.setDateTime(__DATE__, __TIME__);
   //setup rgb led
   pinMode(REDL, OUTPUT);
   pinMode(GREENL, OUTPUT);
@@ -104,8 +107,9 @@ void setup()
   tft.setTextColor(GREEN);  
     tft.setTextSize(3);
     Serial.begin(9600);
-  Serial.println(F("TFT LCD test"));
+  Serial.println(F("TFT LCD test")); 
   tft.fillScreen(BLACK);
+  tft.fillRect(0,0,320,240, BLACK);
 //setup settings button
 #ifdef USE_Elegoo_SHIELD_PINOUT
   Serial.println(F("Using Elegoo 2.4\" TFT Arduino Shield Pinout"));
@@ -159,48 +163,52 @@ void setup()
 
 void loop()
 {
-
+dt = clock.getDateTime();
   //timers
   unsigned long currentMillis = millis();
-//test code
+ //test code
  digitalWrite(13, HIGH);
   TSPoint p = ts.getPoint();
   digitalWrite(13, LOW);
  pinMode(XM, OUTPUT);
   pinMode(YP, OUTPUT);
-  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) {
-    // scale from 0->1023 to tft.width
-    p.x = map(p.y, TS_MINX, TS_MAXX, tft.height(), 0);
-    p.y = (tft.width()-map(p.x, TS_MINY, TS_MAXY, tft.width(), 0));
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) 
+   {
+     // scale from 0->1023 to tft.width
+      p.x = map(p.y, TS_MINX, TS_MAXX, tft.height(), 0);
+      p.y = (tft.width()-map(p.x, TS_MINY, TS_MAXY, tft.width(), 0));
    }
-    if (buttons[0].contains(p.y, p.x)) {
+  
+ for (uint8_t b=0; b<6; b++) 
+  {
+    if (buttons[b].contains(p.x, p.y)) 
+    {
       //Serial.print("Pressing: "); Serial.println(b);
-      buttons[0].press(true); 
-     // tell the button it is pressed
-    } else {
-      buttons[0].press(false);  // tell the button it is NOT pressed
+      buttons[b].press(true);  // tell the button it is pressed
+    } else 
+    {
+      buttons[b].press(false);  // tell the button it is NOT pressed
     }
-  if (buttons[0].justPressed()) {
-        buttons[0].drawButton(true); 
-        if (beep_enabled = true)
-        {
-          beep_enabled = false;
-        }
-        else 
-        {
-          beep_enabled == true;
-        } // draw invert!
   }
-   if (buttons[0].justReleased()) {
-      // Serial.print("Released: "); Serial.println(b);
-      buttons[0].drawButton();  // draw normal
+
+   for (uint8_t b=0; b<6; b++) 
+   {
+    if (buttons[b].justReleased()) 
+    {
+      
+      hightemp++;
+      tft.fillRect(241,210,40,20, BLACK);
+      tft.setCursor(241,210);
+      tft.print(floor(hightemp),0);
     }
- 
+   }
+
+
   tft.setCursor(0,0);
-// print out a sucessfull measurement on lcd 
+ // print out a sucessfull measurement on lcd 
 
   //read temp and humidity
- tempF = dht22.readTemperature();
+ tempF = dht22.readTemperature(true);
  humidity = dht22.readHumidity();
  // send humidity data to dot matrix lcd
  //tempF = temperature * 9.0 / 5.0 + 32.0;
@@ -211,23 +219,36 @@ void loop()
   {
     lastExecutedMillis_1 = currentMillis;
    tft.println("Temperature:");
-    tft.fillRect(0,72,110,25, BLACK);
-  tft.fillRect(rectx,recty,120,26, BLACK);
-    //tft.print(tempF);
-   
+  tft.fillRect(0,29,120,25, BLACK);
+  
+   tft.setCursor(0,30);
     tft.print(tempF);
-    tft.println("C");
+    tft.println("F");
+   tft.setCursor(0,55); 
     tft.println("Humidity:");
-  //   tft.drawRect(rectx,recty,140,50, BLACK);
-
+  
+  tft.fillRect(0,76,120,27, BLACK);
     tft.print(humidity);
     tft.println("%");
-
-
+    tft.setCursor(0,106);
+    tft.println("Date/Time");
+    tft.fillRect(0,130,290,26, BLACK);
+    //tft.print(clock.dateFormat("jS M y, h:ia", dt));
+    tft.setCursor(0,132);
+    tft.print(dt.month);
+    tft.print("/");
+    tft.print(dt.day);
+    tft.print("/");
+    tft.print(dt.year);
+    tft.print(" ");
+    tft.print(dt.hour);
+    tft.print(":");
+    tft.print(dt.minute);
+    
   }
   
  
-//change LED and or beep depending on temperature ranges
+ //change LED and or beep depending on temperature ranges
   if (tempF <= lowtemp && tempF >= coldtemp)
   {
     greenValue = 255;
@@ -238,17 +259,17 @@ void loop()
     analogWrite(BLUEL, blueValue);
     
   }
-  if (tempF <= hightemp && tempF >= lowtemp)
-  {
-    redValue = 255;
-    greenValue= 255;
-    blueValue= 0;
-    analogWrite(REDL, redValue);
-    analogWrite(GREENL, greenValue);
-    analogWrite(BLUEL, blueValue);
+    if (tempF <= hightemp && tempF >= lowtemp)
+    {
+      redValue = 255;
+      greenValue= 255;
+      blueValue= 0;
+      analogWrite(REDL, redValue);
+      analogWrite(GREENL, greenValue);
+      analogWrite(BLUEL, blueValue);
     
-  }
-  if (tempF <= coldtemp)
+    }
+    if (tempF <= coldtemp)
     {
       redValue = 0;
       blueValue = 255;
@@ -256,61 +277,19 @@ void loop()
       analogWrite(REDL, redValue);
       analogWrite(GREENL, greenValue);
       analogWrite(BLUEL, blueValue);
-      if (beep_enabled == true)
-      {   
-        if (currentMillis >= beep_timer)
-        {   
-        for (int thisNote = 0; thisNote < 8; thisNote++) 
-        {
-          // pin8 output the voice, every scale is 0.5 sencond
-          tone(3, melody[thisNote], duration);
-          delay(500);
-          // Output the voice after several minutes
-          
-        }
-    
-        }
-      }
+       
     }
-      if (tempF > hightemp)
+       if (tempF > hightemp)
         { 
           redValue = 255;
           blueValue = 0;
           greenValue = 0;
           analogWrite(REDL, redValue);
           analogWrite(GREENL, greenValue);
-          analogWrite(BLUEL, blueValue);
-          if (beep_enabled == true)
-          {
-            if (currentMillis - lastExecutedMillis_2 >= beep_timer)
-            {
-              lastExecutedMillis_2 >= currentMillis;
-              // pin8 output the voice, every scale is 0.5 sencond
-           
-           
-            if (currentMillis - lastExecutedMillis_3 >= melody_timer)
-            {
-               lastExecutedMillis_3 = currentMillis;
-             tone(speaker, melody[thisNote], duration);
-            
-             if (thisNote <= 8)
-             {
-              thisNote++;
-             }
-             else if(thisNote = 8)
-             {
-              thisNote = 0;
-             }
-            // Output the voice after several minutes
-            }
-            
-    
-          }
-  
+          analogWrite(BLUEL, blueValue); 
     
         }   
-     }
-   }
+     
+}
  
-//} 
-
+ 
